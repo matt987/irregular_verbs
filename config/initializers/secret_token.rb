7006a98648e57f80b2f5bae3c6f807e25144ce49
Carlos Mathiasen <carlos@integrafevs.com>
2013-06-18 13:54:21 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IrregularVerbs::Application.config.secret_token = 'e132dd99accc512163be9ff74e17ff5e95822d4f639db43ea9ef0e435b2202d0efa9cf6d4e7a6debd3e0a2fec275f529e6a1920330472484c2220411c7163dae'
